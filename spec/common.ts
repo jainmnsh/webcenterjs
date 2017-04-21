cb7292d908eff9dd2810d8a6f8dd2fdf441cb47f
Rakesh Gajula <rakesh.gajula@gmail.com>
2017-04-21 00:24:48 -0400

    more unit test, refactored portal.ts

---
+++ spec/common.ts
@@ -0,0 +1,27 @@
+import WebCenter from "../lib";
+export function init(): void {
+    // const restBaseUrl: string = "http://krowddev.darden.com/rest";
+    // const wcBaseUrl: string = "http://krowddev.darden.com/webcenter";
+    // const csBaseUrl: string = "http://krowddev.darden.com/cs";
+
+    const restBaseUrl: string = "http://192.168.1.49:8888/rest";
+    const wcBaseUrl: string = "http://192.168.1.49:8888/webcenter";
+    const csBaseUrl: string = "http://192.168.1.49:16200/cs";
+
+    WebCenter.Config.setRestBaseUrl(restBaseUrl);
+    WebCenter.Config.setWcBaseUrl(wcBaseUrl);
+    WebCenter.Config.setCsBaseUrl(csBaseUrl);
+
+    const username: string = "abarnes";
+    const password: string = "welcome1";
+
+    WebCenter.Auth.setUserName(username);
+    WebCenter.Auth.setPassword(password);
+
+    expect(restBaseUrl).toBe(WebCenter.Config.getRestBaseUrl());
+}
+
+export function logout(): void {
+    WebCenter.Auth.logout();
+    expect(true).toBe(true);
+}
