c1c2a2a1e7ae8c7c380d988599c45ce45b8a11a0
Rakesh Gajula <rakesh.gajula@gmail.com>
2017-04-26 23:58:53 -0400

    minor changes

--- spec/common.ts
+++ spec/common.ts
@@ -1,19 +1,16 @@
 import WebCenter from "../lib";
 export function init(): void {
-    const restBaseUrl: string = "http://krowdstage.darden.com/rest";
-    const wcBaseUrl: string = "http://krowdstage.darden.com/webcenter";
-    const csBaseUrl: string = "http://krowdstage.darden.com/cs";
 
-    // const restBaseUrl: string = "http://192.168.1.49:8888/rest";
-    // const wcBaseUrl: string = "http://192.168.1.49:8888/webcenter";
-    // const csBaseUrl: string = "http://192.168.1.49:16200/cs";
+    const restBaseUrl: string = "http://192.168.1.49:8888/rest";
+    const wcBaseUrl: string = "http://192.168.1.49:8888/webcenter";
+    const csBaseUrl: string = "http://192.168.1.49:16200/cs";
 
     WebCenter.Config.setRestBaseUrl(restBaseUrl);
     WebCenter.Config.setWcBaseUrl(wcBaseUrl);
     WebCenter.Config.setCsBaseUrl(csBaseUrl);
 
-    const username: string = "880690388";
-    const password: string = "Darden88";
+    const username: string = "abarnes";
+    const password: string = "welcome1";
 
     WebCenter.Auth.setUserName(username);
     WebCenter.Auth.setPassword(password);
